d0a7edb496ed7aeb5021ed889c70a11aafef042b
Neeraj Mani <neerajmani456@gmail.com>
2023-04-03 11:33:46 +0530

    final-commit

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ let signupForm = document.querySelector(".signup-form");
 let emailField = document.querySelector(".email");
 let fullNameField = document.querySelector(".fullName");
 let passField = document.querySelector(".pass");
+let passEye = signupForm.querySelector(".eye");
+let pricingCardList = document.querySelectorAll(".pricing-card");
+let pricingContainer = document.querySelector(".pricing-container");
+let coursesContainer = document.querySelector(".courses-container");
+let signupContainer = document.querySelector(".signup-container");
 
 window.addEventListener("scroll", () => {
   header.classList.toggle("shadow", window.scrollY > 0);
@@ -43,3 +48,38 @@ signupForm.addEventListener("submit", (e) => {
   }
   alert("Signed up successfully!");
 });
+
+passEye.addEventListener("click", (e) => {
+  if (passField.type === "text") {
+    passField.type = "password";
+    passEye.classList.toggle("bar");
+    return;
+  }
+  passField.type = "text";
+  passEye.classList.toggle("bar");
+});
+
+pricingContainer.addEventListener("click", (e) => {
+  pricingCardList.forEach((card) => {
+    card.classList.remove("card-selected");
+  });
+  e.target.closest(".pricing-card").classList.add("card-selected");
+});
+
+let observer = new IntersectionObserver(
+  (entries, observer) => {
+    console.log(entries);
+    if (entries[0].isIntersecting) {
+      entries[0].target.classList.remove("animate");
+      observer.unobserve(entries[0].target);
+    }
+  },
+  {
+    root: null,
+    threshold: 0.3,
+  }
+);
+
+observer.observe(pricingContainer);
+observer.observe(coursesContainer);
+observer.observe(signupContainer);
